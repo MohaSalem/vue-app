163189bf859518df27a59dc9411ac53041aa02be
MohaSalem <mohammad.ramadan.mahmoud@gmail.com>
2021-05-16 23:24:19 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Web applications subject final project
+
+## Project setup
+
+```
+npm install
+```
+
+```
+npm run serve
+```
+
+### For production
+
+```
+npm run build
+```
+
+# This is a school task
---
+++ src/components/HelpPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="help-page">
+    Useful information for customers help!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelpPage',
+  components: {}
+}
+</script>
+
+<style scoped>
+</style>
